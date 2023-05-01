e9e710d43785980e8ad91fd941772e682aa62df6
daniel <dvds1987@gmail.com>
2023-05-01 13:24:15 +0200

    Create reset password page

---
+++ src/pages/auth/reset-password/ResetPassword.js
@@ -0,0 +1,67 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import { FaArrowLeft } from "react-icons/fa";
+import Input from "../../../components/input/Input";
+import Button from "../../../components/button/Button";
+import "./ResetPassword.scss";
+
+const ResetPassword = () => {
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [loading] = useState(false);
+
+  return (
+    <div className="container-wrapper">
+      <div className="container-wrapper-auth">
+        <div className="tabs reset-password-tabs">
+          <div className="tabs-auth">
+            <ul className="tab-group">
+              <li className="tab">
+                <div className="login reset-password">Reset Password</div>
+              </li>
+            </ul>
+            <div className="tab-item">
+              <div className="auth-inner">
+                <form className="reset-password-form">
+                  <div className="form-input-container">
+                    <Input
+                      id="password"
+                      name="password"
+                      type="password"
+                      value={password}
+                      labelText="New Password"
+                      placeholder="New Password"
+                      handleChange={(e) => setPassword(e.target.value)}
+                    />
+                    <Input
+                      id="cpassword"
+                      name="cpassword"
+                      type="password"
+                      value={confirmPassword}
+                      labelText="Confirm Password"
+                      placeholder="Confirm Password"
+                      handleChange={(e) => setConfirmPassword(e.target.value)}
+                    />
+                  </div>
+                  <Button
+                    label={`${loading ? "RESET PASSWORD IN PROGRESS..." : "RESET PASSWORD"}`}
+                    className="auth-button button"
+                    disabled={!password || !confirmPassword}
+                  />
+
+                  <Link to={"/app/login"}>
+                    <span className="login">
+                      <FaArrowLeft className="arrow-left" /> Back to Login
+                    </span>
+                  </Link>
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ResetPassword;
--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,7 @@ import { useRoutes } from "react-router-dom";
 import Home from "./pages/home/Home";
 import { AuthTabs } from "./pages/auth";
 import ForgotPassword from "./pages/auth/forgot-password/ForgotPassword";
+import ResetPassword from "./pages/auth/reset-password/ResetPassword";
 
 export const Routes = () => {
   const elements = useRoutes([
@@ -16,6 +17,10 @@ export const Routes = () => {
     {
       path: "/app/forgot-password",
       element: <ForgotPassword />
+    },
+    {
+      path: "/reset-password",
+      element: <ResetPassword />
     }
   ]);
   return elements;
